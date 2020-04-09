18e3210e90edc2cd0131438af9bbb62b38757f16
Kasia Paschilke <kpaschilke@gmail.com>
2020-04-09 18:23:51 +0100

    Created functions show list and add task.

--- task_man.py
+++ task_man.py
@@ -1,3 +1,9 @@
+#variables
+
+list_of_tasks = []
+
+#functions
+
 def print_menu():
     
     gap_size = 5
@@ -9,6 +15,35 @@ def print_menu():
     print(gap + "4.Remove")
     print(gap + "5.Exit\n")
     
+def show_list():
+    
+    global list_of_tasks
+    if len(list_of_tasks) == 0:
+        print("Your list is empty! Pick option 3 to add your first task.\n")
+    else:
+        for i, task in enumerate(list_of_tasks):
+            i = i + 1
+            print(str(i) + ". " + task)
+        
+def show_details():
+    pass
+
+def add_task():
+    
+    
+    global list_of_tasks
+   
+    while True:
+        task = input("Please type your task or leave it empty to quit.\n")
+        if task == "":
+            break
+        else:
+            list_of_tasks.append(task)
+            print("Your task \"" + task + "\" has been submitted.\n")
+            
+def remove_task():
+    pass
+    
 if __name__ == "__main__":
     
     print("Hello! Welcome to your personal task manager!")
@@ -16,5 +51,18 @@ if __name__ == "__main__":
     while True:
         print_menu()
         option = input("Please pick the option by typing a number from 1-5\n")
-        if option == "5":
+        print()
+        if option == "1":
+            show_list()
+            
+        elif option == "2":
+            show_details()
+            
+        elif option == "3":
+            add_task()
+        
+        elif option == "4":
+            remove_task()
+        
+        elif option == "5":
             break
