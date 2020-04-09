5e628ff6cd460f8d75a40fbcfad7f4124820de73
Kasia Paschilke <kpaschilke@gmail.com>
2020-04-09 17:35:09 +0100

    Moved menu into function.
    Also added __main__.

--- task_man.py
+++ task_man.py
@@ -1,15 +1,20 @@
-print("Hello! Welcome to your personal task manager!")
-
-gap_size = 5
-gap = " " * gap_size
+def print_menu():
     
-while True:
+    gap_size = 5
+    gap = " " * gap_size
     print(" " * 10 + "MENU\n")
     print(gap + "1.List")
     print(gap + "2.Details")
     print(gap + "3.Add")
     print(gap + "4.Remove")
     print(gap + "5.Exit\n")
-    option = input("Please pick the option by typing a number from 1-5\n")
-    if option == "5":
-        break
+    
+if __name__ == "__main__":
+    
+    print("Hello! Welcome to your personal task manager!")
+    
+    while True:
+        print_menu()
+        option = input("Please pick the option by typing a number from 1-5\n")
+        if option == "5":
+            break
